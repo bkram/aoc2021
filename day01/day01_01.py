f208a26d5e83b5c1da05e3e721f4f72086993a20
Mark de Bruijn <mrdebruijn@gmail.com>
2021-12-01 16:44:36 +0100

    Add Day 1

---
+++ day01/day01_01.py
@@ -0,0 +1,10 @@
+counter = 0
+previous = 0
+match = 0
+with open('input', 'r') as file:
+    data = file.read().split('\n')[1:-1]
+    for a in data:
+        if int(a) > previous:
+            match += 1
+        previous = int(a)
+    print('Higher {}'.format(match))
