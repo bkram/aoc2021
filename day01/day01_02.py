7fb18ba3398e11a2c579f498a89faafcf9b176c5
Mark de Bruijn <mrdebruijn@gmail.com>
2021-12-01 16:56:23 +0100

    Add Day 1, remove unneeded code

--- day01/day01_02.py
+++ day01/day01_02.py
@@ -1,14 +1,12 @@
-counter = 0
 previous = 0
 match = 0
 with open('input', 'r') as file:
     data = file.read().split('\n')[:-1]
-    for i in range(0, len(data)):
-        if not i == 0:
-            this = data[i:(i + 3)]
-            if len(this) == 3:
-                window = int(this[0]) + int(this[1]) + int(this[2])
-                if window > previous:
-                    match += 1
-                previous = window
+    for i in range(1, len(data)):
+        this = data[i:(i + 3)]
+        if len(this) == 3:
+            window = int(this[0]) + int(this[1]) + int(this[2])
+            if window > previous:
+                match += 1
+            previous = window
     print('Higher windows {}'.format(match))
